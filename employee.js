f519cc6a045eb36c278ccf28a78a74e13c8d0d4c
Qasim223 <41677739+Qasim223@users.noreply.github.com>
2022-02-11 13:32:38 +0000

    Vue Project Start

---
+++ employee.js
@@ -0,0 +1,1 @@
+const employee = { template: `<h1>This is the Employee Page</h1>` }
---
+++ variables.js
@@ -0,0 +1,9 @@
+const variables = {
+    API_URL: "http://localhost:5119/api/",
+    PHOTO_URL: "http://localhost:5119/photos/"
+}
+
+const options = {
+    headers: { 'Content-Type': 'application/json'}
+  };
+  
